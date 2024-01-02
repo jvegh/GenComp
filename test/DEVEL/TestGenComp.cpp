a679439569049690e7e5a5a9e272ea3404b76623
jvegh <Vegh.Janos@gmail.com>
2024-01-02 20:45:02 +0100

    Testing restricted to PU_Bio

--- test/DEVEL/TestGenComp.cpp
+++ test/DEVEL/TestGenComp.cpp
@@ -7,7 +7,7 @@
 #undef DEBUGGING
 #undef SUPPRESS_LOGGING
 
-#include <sstream>
+//#include <sstream>
 
 /** @class	GenCompTest
  * @brief	Tests the operation of the objects for generalized computing
@@ -19,8 +19,8 @@ extern bool UNIT_TESTING;		// Switched off by default
 #include "scGenComp_PU_Bio.h"
 #include "scGenComp_PU_Tech.h"
 
-//scAbstractGenComp_PU PU("myAbs");
-//scTechGenComp_PU TPU("myTech",2);
+/*scGenComp_PU_Abstract PU("myAbs");
+scGenComp_PU_Tech TPU("myTech",2);*/
 scGenComp_PU_Bio BPU("myBio");
 
 // A new test class  of these is created for each test
@@ -39,27 +39,35 @@ public:
     }
 };
 
-#if 0
 /**
  * Tests the abstract processing unit
  */
+#if 0
 TEST_F(GenCompTest, AbstractPU)
 {
-    scAbstractGenComp_PU* MyPU = &PU;
+    scGenComp_PU_Abstract* MyPU = &PU;
     EXPECT_EQ( gcsm_Ready,MyPU->StateFlag_Get());  // The unit is initialized to 'Ready' state
 
     // Attempt to pass to 'Processing'
+    sc_core::sc_time sct_base = sc_time_stamp();
     MyPU->EVENT_GenComp.InputReceived.notify(10,SC_US);
     wait(MyPU->EVENT_GenComp.InputReceived);
- /*   PU.EVENT_GenComp.Sleep.notify(SC_ZERO_TIME);
+    // Now we should be at 10 us simulated time
+    EXPECT_EQ(sct_base + sc_core::sc_time(10,SC_US),sc_core::sc_time_stamp());
+    MyPU->EVENT_GenComp.InputReceived.notify(10,SC_US);
+//    MyPU->EVENT_GenComp.Heartbeat.notify(SC_ZERO_TIME);
+//    EXPECT_DEATH(wait(MyPU->EVENT_GenComp.Heartbeat),"");    //Attempt to send the unit to sleep again
+ //   EXPECT_EQ(gcsm_Ready, PU.StateFlag_Get());  // Remains ready, given that AbstractPU.Process failed
+}
+/*  Not yet implemented
+ *   PU.EVENT_GenComp.Sleep.notify(SC_ZERO_TIME);
     wait(PU.EVENT_GenComp.Sleep);
     //Send the unit to sleep in 'Ready' state
     EXPECT_EQ(gcsm_Sleeping,MyPU->StateFlag_Get()); // Now it is sleeping
     EXPECT_DEATH(MyPU->Sleep_method(),"");    //Attempt to send the unit to sleep again
 */
- //   EXPECT_EQ(gcsm_Ready, PU.StateFlag_Get());  // Remains ready, given that AbstractPU.Process failed
-}
-#endif // 0
+#endif
+
 /**
  * Tests the generic biological processing unit
  */
@@ -69,24 +77,26 @@ TEST_F(GenCompTest, BioPU)
     EXPECT_EQ( gcsm_Ready, MyBPU->StateFlag_Get());  // The unit is initialized to 'Ready' state
     // Now the 1st spike arrives
     BPU.EVENT_GenComp.InputReceived.notify(SC_ZERO_TIME);     // The BPU starts to receive spikes
-    wait(BPU.EVENT_GenComp.Begin_Computing);
+    wait(BPU.EVENT_GenComp.ProcessingBegin);
     EXPECT_EQ( gcsm_Processing, MyBPU->StateFlag_Get());  // The unit goes to 'Processing' state
-    wait(BPU.EVENT_GenComp.HeartBeat);
+    wait(BPU.EVENT_GenComp.Heartbeat);
     BPU.EVENT_GenComp.InputReceived.notify(SC_ZERO_TIME);     // The BPU starts to receive spikes
-    wait(BPU.EVENT_GenComp.HeartBeat);
-    wait(BPU.EVENT_GenComp.End_Computing);
+    wait(BPU.EVENT_GenComp.ProcessingEnd);
     EXPECT_EQ( gcsm_Delivering, MyBPU->StateFlag_Get());  // The unit goes to 'Delivering' state
+    wait(BPU.EVENT_GenComp.DeliveringEnd);
+    EXPECT_EQ( gcsm_Relaxing, MyBPU->StateFlag_Get());  // The unit goes to 'Delivering' state
 
 }
 
 /**
  * Tests the generic technical processing unit
-
+*/
+#if 0
 TEST_F(GenCompTest, TechPU)
 {
-    scTechGenComp_PU* MyTPU = &TPU;
+    scGenComp_PU_Tech* MyTPU = &TPU;
      EXPECT_EQ( gcsm_Ready, MyTPU->StateFlag_Get());  // The unit is initialized to 'Ready' state
 //   TPU.Process_method();                         // The TPU starts to 'process'
 //     EXPECT_EQ( gcsm_Processing, MyTPU->StateFlag_Get());  // The unit is goes to 'Processing' state
 }
-*/
+#endif
