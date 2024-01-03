3392e1208aa6478954e7f8ec8b43c008e55a6bd4
jvegh <Vegh.Janos@gmail.com>
2024-01-03 09:24:52 +0100

    Time measuring macros completed

--- modules/include/MacroTimeBenchmarking.h
+++ modules/include/MacroTimeBenchmarking.h
@@ -1,157 +1,113 @@
 /*! @file MacroTimeBenchmarking.h
-
- *  Macros for execution (clock) time handling in benchmarks
- */
-/*
- *  @author János Végh (jvegh)
+ *  @brief Macros for (clock) execution time (simulated) handling in benchmarks
  *  @ingroup GENCOMP_MODULE_STUFF
- *  @bug No known bugs.
- */
+ *
+    These defined macros are the benchmarking macros for making computing (clock) time measurements.
+    The "MacroTimeBenchmarking.h" file shall be included in the header of the module in question.
+    The results of the time measurements will be returned in user-defined variables.
+    As the arguments are user-provided, with consistent use several independent
+    measurement can be carried out within the source scope. The macros have source-module scope.
+    All variables must be passed by reference.
 
-/*!
-@verbatim
-    This module defines the benchmarking macros for making computing (clock) time measurements.
-    This file shall be included in the header of the module in question.
+    Notice: this is CLOCK time, returned by the processor; not SIMULATED time.
+
+    The operating time offset due to benchmarking is approx 55-85 nanosecs, per macro pairs.
+
+<b>Usage:</b>
     If the module includes this header, it must be preceded by defining
+    @code{.cpp}
+#define MAKE_TIME_BENCHMARKING  // uncomment to measure the time with benchmarking macros
+    @endcode
+   If this name is not defined, no code is generated for the module.
+    The variables, however, must be defined (although they will be
+    optimized out as unused ones).
+    Alternatively, generating those variables may be protected with
+    @code{.cpp}
+#ifdef MAKE_TIME_BENCHMARKING
+    @endcode
 
-    MAKE_TIME_BENCHMARKING
+<b>Example:</b>
+    in your module, between the '\#include' files write
 
-    If it is not defined, no code is generated. The variables, however,
-    must be defined. They will be optimized out as unused ones,
-    or they may be protected with
-
-    "\#ifdef MAKE_TIME_BENCHMARKING"
-
-    The returned values are
-
-    'std::chrono::duration<int64_t, nano> x',
-
-    can be accessed as
-
-    x.count()
-
-    in nanoseconds, as int64_t values.
-
-
-    That is: in your module, between the '\#include' files write
-
-\#define MAKE_TIME_BENCHMARKING  // comment out if you do not want to benchmark
-\#include "MacroTimeBenchmarking.h"    // Must be after the define to have its effect
-
-    In your class (or member function) define
-
-    chrono::steady_clock::time_point t;
-    std::chrono::duration<int64_t, nano> x,s=(std::chrono::duration< int64_t, nano>)0;
-
-    and use the macros as as
-
-    BENCHMARK_TIME_RESET(&t,&x,&s); // Reset at the very begining, say in the constructor
-
-Later put the benchmarked code between macros, used as brackets
-
-    BENCHMARK_TIME_BEGIN(&t,&x);    // Begin the benchmarking here
-    BENCHMARK_TIME_END(&t,&x,&s);   // End benchmarking here
-
-    Now you have the benchmarked time (since BEGIN) in x (nanoseconds)
-    and the sum of all benchmarked time (since RESET) in s (nanoseconds)
-
-    I.e. use it like
-    std::cerr  << "Simulation took " << s.count()/1000/1000. << " msec CLOCK time" << endl;
-
-    Notice: this is CLOCK time, returned by the processor; not SIMULATED time
-@endverbatim
- */
-
-
-  \def BENCHMARK_TIME_RESET(t,x,s)
-@verbatim
-  Clears the user-provided \a x and \a s duration values
-  Initializes the user-provided  \a t timepoint
-
-    This macro group defines the benchmarking macros for making time measurements.
-    The file "MacroTimeBenchmarking.h" shall be included in the header of the module
-    where the benchmark is to be used.
-    If the module includes this header, and it is preceded by
-    '\#define MAKE_TIME_BENCHMARKING',
-    the results of the time measurements will be returned in the user-defined variables.
-    If this macro is not defined, no code is generated;
-  the variables, however, must be defined (although they will be
-  optimized out as unused ones).
-  Alternatively, the macros may be protected with "\#ifdef MAKE_TIME_BENCHMARKING".
-  The macros have source-module scope. All variables must be passed by reference.
-
-  The returned values are of type 'std::chrono::duration< int64_t, nano> x',
-  their value can be accessed as 'x.count()' in nanoseconds, as int64_t values.
-  ('t' is a don't care, BUT DON'T CHANGE, value; contains internal tick values).
-
-  Usage:
-    In your class (or member function) define
-
-    That is: in your module, between the '\#include' files write
     @code{.cpp}
 #define MAKE_TIME_BENCHMARKING  // comment out if you do not want to benchmark
 #include "MacroTimeBenchmarking.h"    // Must be after the define to have its effect
     @endcode
-    You should also define variables (such as)
 
+     In your class (or member function) define
     @code{.cpp}
     chrono::steady_clock::time_point t =chrono::steady_clock::now();
     std::chrono::duration< int64_t, nano> x,s=(std::chrono::duration< int64_t, nano>)0;
     @endcode
-
-    Before starting using the macros, you shall initialize them:
+    and use the macros as as
     @code{.cpp}
     BENCHMARK_TIME_RESET(&t,&x,&s); // Reset at the very begining, say in the constructor
     @endcode
-and later put the benchmarked code between macros, used as brackets
+    Later put the benchmarked code between macros, used as brackets
     @code{.cpp}
     BENCHMARK_TIME_BEGIN(&t,&x);    // Begin the benchmarking here
     BENCHMARK_TIME_END(&t,&x,&s);   // End benchmarking here
     @endcode
-
-    After using these macros, the benchmarked time are returned:
+    After using these macros, the benchmarked time values are returned:
     (since BEGIN) in x (nanoseconds)
     and the sum of all benchmarked time (since RESET) in s (nanoseconds).
-    For example, in an object, you can RESET in the constructor,
+    Access the result as
+    @code{.cpp}
+    std::cerr  << "Simulation took " << s.count()/1000/1000. << " msec CLOCK time" << endl;
+    @endcode
+
+In an object, you can RESET in the constructor,
     in the member functions between BEGIN and END measure the
     one-time utilization, and in the destructor to read out the total benchmarked time.
+ */
+/*  @author János Végh (jvegh)
+ *  @bug No known bugs.
+ */
 
-    The offset due to benchmarking is approx 55-85 nanosecs
-
-    As the arguments are user-provided, with consistent use several independent
-    measurement can be carried out within the source scope.
-@endverbatim
-
-    @see BENCHMARK_TIME_RESET
-    @see BENCHMARK_TIME_BEGIN
-    @see BENCHMARK_TIME_END
-*/
 
 
 /*!
   \def BENCHMARK_TIME_BEGIN(t,x)
-  Changes the user-provided  \a t timepoint
+  Changes the user-provided  \a t timepoint.
   Sets the user-provided \a x to the last TIME_BEGIN \a t value
+  @param t for internal use, the absolute beginning of time  (do not touch)
+  @param x deliver clock time of the beginning
 */
 
 /*!
   \def BENCHMARK_TIME_END(t,x,s)
   Changes the user-provided  \a t timepoint.
   Sets the user-provided \a x to the duration since the last TIME_BEGIN \a t value.
-  Sets the user-provided \a s to the duration since the last TIME_RESET \a t value.
+  Sets the user-provided ime of the beginning\a s to the duration since the last TIME_RESET \a t value.
+  @param t for internal use, the absolute beginning of time  (do not touch)
+  @param x deliver clock duration since _BEGIN (only read)
+  @param s deliver clock duration since _RESET (only read)
 */
+
+/*!
+  \def BENCHMARK_TIME_RESET(t,x,s)
+  Clears the user-provided \a x and \a s duration values
+  Sets the user-provided \a x to the duration since the last TIME_BEGIN \a t value.
+  Sets the user-provided \a s to the duration since the last TIME_RESET \a t value.
+  @param t for internal use, the absolute beginning of time (do not touch)
+  @param x deliver clock duration since _BEGIN (only read)
+  @param s deliver clock duration since _RESET (only read)
+*/
+
 
 #ifdef MAKE_TIME_BENCHMARKING
 #include <ctime>
 #include <ratio>
 #include <chrono>
 
-// Return the time since the last call in x, and sums the elapsed time in s
+// Return the time since the last call TIME_BEGIN in x, and sums the elapsed time in s
 #define BENCHMARK_TIME_END(t,x,s)\
     *x = std::chrono::duration< int64_t, nano>(std::chrono::steady_clock::now() - *t); \
     *t = std::chrono::steady_clock::now(); *s += *x;
+// Initializes measurement summary
 #define BENCHMARK_TIME_RESET(t,x,s)\
     BENCHMARK_TIME_BEGIN(t,x); *s=*x;
+// Remembers beginning of time
 #define BENCHMARK_TIME_BEGIN(t,x)\
     *t = std::chrono::steady_clock::now(); *x=*t-*t;
 #else // The time measurement not needed, do nothing
--- modules/include/scMacroTimeBenchmarking.h
+++ modules/include/scMacroTimeBenchmarking.h
@@ -1,108 +1,103 @@
 /** @file scMacroTimeBenchmarking.h
- *  @ingroup GENCOMP_MODULE_PROCESS
  *  @brief Macros for SystemC  (simulated) execution time (simulated) handling in benchmarks
- */
-/*  @author János Végh (jvegh)
- *  @bug No known bugs.
- */
-/*!
-@verbatim
-    This macro group defines the benchmarking macros for making simulated time measurements.
-    The file "scMacroTimeBenchmarking.h" shall be included in the header of the module
-    where the benchmark is to be used.
+ *  @ingroup GENCOMP_MODULE_STUFF
+ *
+    The "scMacroTimeBenchmarking.h" file shall be included in the header of the module in question.
+    The results of the time measurements will be returned in user-defined variables.
+    As the arguments are user-provided, with consistent use several independent
+    measurement can be carried out within the source scope. The macros have source-module scope.
+    All variables must be passed by reference.
+
+    Notice: this is SIMULATED  time, returned by the SystemC engine; not CLOCK time returned by the processor.
+
+    The operating time offset due to benchmarking is approx 55-85 nanosecs, per macro pairs.
+
+
+<b>Usage:</b>
     If the module includes this header, it must be preceded by defining
-
-    If the module includes this header, and it is preceded by
-    '\#define SC_MAKE_TIME_BENCHMARKING',
-    the results of the time measurements will be returned in the user-defined variables.
-
-    If this macro is not defined, no code is generated.
+    @code{.cpp}
+#define SC_MAKE_TIME_BENCHMARKING  // uncomment to measure the time with benchmarking macros
+    @endcode
+   If this name is not defined, no code is generated for the module.
     The variables, however, must be defined (although they will be
-  optimized out as unused ones).
-  Alternatively, the macros may be protected with
-  '\#ifdef SC_MAKE_TIME_BENCHMARKING'.
-  The macros have source-module scope. All variables must be passed by reference.
-
-
-
-    can be accessed as
-
-    sc_time_String_Get(SC_TIME_UNIT_DEFAULT,SC_x)
-
-    in nanoseconds, as int64_t values.
-
-    That is: in your module, between the '\#include' files write
-
-       @code{.cpp}
-\#define SC_MAKE_TIME_BENCHMARKING  // comment out if you do not want to benchmark
-\#include "scMacroTimeBenchmarking.h"    // Must be after the define to have its effect
+    optimized out as unused ones).
+    Alternatively, generating those variables may be protected with
+    @code{.cpp}
+#ifdef MAKE_TIME_BENCHMARKING
     @endcode
 
+<b>Example:</b>
+    in your module, between the '\#include' files write
 
-    In your class (or member function) define
+    @code{.cpp}
+#define SC_MAKE_TIME_BENCHMARKING  // comment out if you do not want to benchmark
+#include "scMacroTimeBenchmarking.h"    // Must be after the define to have its effect
+    @endcode
 
+     In your class (or member function) define
     @code{.cpp}
     sc_core::sc_time SC_t, SC_x, SC_s;
     @endcode
-
-    Before starting using the macros, you shall initialize them:
+    and use the macros as as
     @code{.cpp}
     SC_BENCHMARK_TIME_RESET(&SC_t,&SC_x,&SC_s); // Reset at the very begining, say in the constructor
     @endcode
-    and use the macros as as
-
-and later put the benchmarked code between macros, used as brackets
+    Later put the benchmarked code between macros, used as brackets
     @code{.cpp}
     SC_BENCHMARK_TIME_BEGIN(&SC_t,&SC_x);    // Begin the benchmarking here
     SC_BENCHMARK_TIME_END(&SC_t,&SC_x,&SC_s);   // End benchmarking here
     @endcode
-
-    Later put the benchmarked code between macros, used as brackets
-
-
-    The returned values are
-
-    'sc_core::sc_time x',
-    Now you have the benchmarked time (since BEGIN) in SC_x (nanoseconds)
-    and the sum of all benchmarked time (since RESET) in SC_s (nanoseconds)
-    For example, in an object, you can RESET in the constructor,
-    in the member functions between BEGIN and END measure the
-    one-time utilization, and in the destructor to read out the total benchmarked time.
-
+    After using these macros, the benchmarked time value (since BEGIN) x is returned:
+    and the sum of all benchmarked time (since RESET) in s (nanoseconds).
+    Access the result as
+    @code{.cpp}
     std::cerr << std::fixed << std::setfill (' ') << std::setprecision(d) << std::setw(w)
               << SC_x.to_seconds()*1000.;
-
-    The offset due to benchmarking is approx 55-85 nanosecs
-
-    As the arguments are user-provided, with consistent use several independent
-    measurement can be carried out within the source scope.
-
-    Notice: this is SIMULATED time, returned by SystemC engine; nor CLOCK time
-@endverbatim
+    @endcode
+    or, if confortable
+    can be accessed as
+    @code{.cpp}
+    sc_time_String_Get(SC_TIME_UNIT_DEFAULT,SC_x)
+    @endcode
+ */
+/*  @author János Végh (jvegh)
+ *  @bug No known bugs.
  */
 
-#ifdef MAKE_SCTIME_BENCHMARKING
-#include <systemc>
-
 /*!
-  \def BENCHMARK_TIME_END(t,x,s)
+  \def SC_BENCHMARK_TIME_END(t,x,s)
   Changes the user-provided  \a t timepoint.
   Sets the user-provided \a x to the duration since the last TIME_BEGIN \a t value.
   Sets the user-provided \a s to the duration since the last TIME_RESET \a t value.
   ('t' is a don't care, BUT DON'T CHANGE, value).
   // Variables must be passed by reference
 */
-#define SC_BENCHMARK_TIME_END(t,x,s)\
-    *x = sc_time_stamp() - *t; \
-    *t = sc_time_stamp(); *s += *x;
 
-#define SC_BENCHMARK_TIME_RESET(t,x,s)\
-    SC_BENCHMARK_TIME_BEGIN(t,x); *s=*x;
 /*!
   \def SC_BENCHMARK_TIME_BEGIN(t,x)
   Changes the user-provided  \a t timepoint
   Sets the user-provided \a x to the last TIME_BEGIN \a t value
 */
+
+/*!
+  \def SC_BENCHMARK_TIME_RESET(t,x,s)
+  Clears the user-provided \a x and \a s duration values
+  Sets the user-provided \a x to the duration since the last TIME_BEGIN \a t value.
+  Sets the user-provided \a s to the duration since the last TIME_RESET \a t value.
+  @param t for internal use, the absolute beginning of time (do not touch)
+  @param x deliver clock duration since TIME_BEGIN (only read)
+  @param s deliver clock duration since TIME_RESET (only read)
+*/
+
+#ifdef SC_MAKE_SCTIME_BENCHMARKING
+#include <systemc>
+#define SC_BENCHMARK_TIME_END(t,x,s)\
+*x = sc_time_stamp() - *t; \
+    *t = sc_time_stamp(); *s += *x;
+
+#define SC_BENCHMARK_TIME_RESET(t,x,s)\
+SC_BENCHMARK_TIME_BEGIN(t,x); *s=*x;
+
 #define SC_BENCHMARK_TIME_BEGIN(t,x)\
     *t = sc_time_stamp(); *x=sc_time(SC_ZERO_TIME);
 #else // The time measurement not needed, do nothing
