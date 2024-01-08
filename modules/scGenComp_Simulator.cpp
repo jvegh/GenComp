f52ac228af916839a909defb7408f011d4c8ab6c
jvegh <Vegh.Janos@gmail.com>
2024-01-08 09:38:32 +0100

    Update mechanism added

--- modules/scGenComp_Simulator.cpp
+++ modules/scGenComp_Simulator.cpp
@@ -15,96 +15,144 @@ extern bool UNIT_TESTING;	// Whether in course of unit testing
 string GenCompSimulatorModesStrings[]{};
 scGenComp_Simulator::scGenComp_Simulator(sc_core::sc_module_name nm)
     : sc_core::sc_module( nm)
-    ,mLocalTimeBase(sc_core::SC_ZERO_TIME)
     ,mToReset(true)             //
+    ,mMoreEvents(true)
 {   // Initialize CLOCK and SIMULATED time counters
     BENCHMARK_TIME_RESET(&t,&x,&s);
     SC_BENCHMARK_TIME_RESET(&SC_t,&SC_x,&SC_s);
+//    SC_OldT = SC_t;
 }
+
+/* Reset the simulator for a new processing
+     */
+void scGenComp_Simulator::
+    Reset()
+{
+    mToReset = false;
+    mResetTime = sc_core::sc_time_stamp();
+    for(vector<scGenComp_PU_Abstract*>::size_type i = 0; i != mWatchedPUs.size(); i++)
+    {
+        mWatchedPUs[i]->CancelEvents(); // Cancel all pending event for this module
+        mWatchedPUs[i]->Initialize_Do(); // Initialize
+    }
+    DEBUG_SC_EVENT("Simulator reset");
+ }
 
 
 bool scGenComp_Simulator::Run(GenCompSimulatorModes_t SimulatorMode, int32_t SimulationUnit)
 {
+    BENCHMARK_TIME_BEGIN(&t,&x);
+    SC_BENCHMARK_TIME_BEGIN(&SC_t,&SC_x);
+    SC_time = sc_core::sc_time_stamp();    // Remember the time of calling, for 'gcsm_Timed' mode
     if(mToReset)
     {
-        mToReset = false;
+        Reset();
         sc_start(SC_ZERO_TIME);  // We are at the beginning, just make a call to set up the SystemC engine
-
+        mMoreEvents = true;
     }
-    scLocalTime_Set();
-    bool ToContinue = true; // Anyhow, process at least once
-    bool MoreEvents = true;
-    while(ToContinue)
-    {
-         // We are in the process of simulating
-        // Run up to the next activity}
-        sc_start( sc_time_to_pending_activity() );
-        sc_pause(); // Be sure there is no processing
-        MoreEvents = sc_pending_activity();    // Set flag if more to do
-        int32_t AA = -12;
-        mWatchedPUs[0]->GetData(AA);
-        ToContinue = MoreEvents;
-
-        BENCHMARK_TIME_BEGIN(&t,&x);
-        SC_BENCHMARK_TIME_BEGIN(&SC_t,&SC_x);
-/*#ifdef SC_BENCHMARK_TIME_BEGIN
-        DEBUG_SC_EVENT("Simulator time stamp at begin " << sc_time_String_Get(sc_time_stamp()));
-        DEBUG_SC_EVENT("Simulator time stamp " << sc_time_String_Get(sc_time_stamp()));
-        DEBUG_SC_EVENT("Simulator begin " << sc_time_String_Get(SC_t));
-        DEBUG_SC_EVENT("Simulator length " << sc_time_String_Get(SC_x));
-#endif*/
-        if(ToContinue)
+    bool MoreCycles = true; // Anyhow, we make one cycle
+    while(MoreCycles && mMoreEvents)  // Until cycling allowed and have ecents
+    {   // We are in the process of simulating
+        sc_start( sc_time_to_pending_activity());   // Run up to the next activity
+        mMoreEvents = sc_pending_activity();    // Check if we have anything to do
+        Update();
+        if((MoreCycles))
         {   // We have still more events, see which mode requested
             switch(SimulatorMode)
             {
                 default:
                 case gcsm_Continuous:
+                    MoreCycles = false;
                     break;        ///< Runs to the end
                 case gcsm_Eventwise:         ///< Stops after a certain number of events
-                  ToContinue = (--SimulationUnit > 0);    // Count events
+                    MoreCycles = (--SimulationUnit > 0);    // Count events
                     break;
 
                 case gcsm_Timed:
- /*                   DEBUG_SC_EVENT("Simulator time stamp " << sc_time_String_Get(sc_time_stamp()));
-                    DEBUG_SC_EVENT("Simulator local time t " <<  sc_time_String_Get(sc_time_stamp()-SC_t));
-                    DEBUG_SC_EVENT("Simulator local time x " <<  sc_time_String_Get(sc_time_stamp()-SC_x));
-                    DEBUG_SC_EVENT("Simulator time step " << sc_time_String_Get(SimulationUnit*HEARTBEAT_TIME_DEFAULT_BIO));
-*/
-                     ToContinue = sc_core::sc_time_stamp() - SC_t < SimulationUnit*HEARTBEAT_TIME_DEFAULT_BIO;
+                    MoreCycles = sc_core::sc_time_stamp() - SC_time <= SimulationUnit*HEARTBEAT_TIME_DEFAULT_BIO;
                     break;
             }
-//                DEBUG_SC_EVENT("Simulator local time x " <<  sc_time_String_Get(sc_time_stamp()-SC_x));
         }
-        BENCHMARK_TIME_END(&t,&x,&s);
-        SC_BENCHMARK_TIME_END(&SC_t,&SC_x,&SC_s);
-//        DEBUG_SC_EVENT("Simulator local time x " <<  sc_time_String_Get(sc_time_stamp()-SC_x));
-
-//                DEBUG_SC_EVENT("Simulator step done") ;
     }
-
-#ifdef SC_BENCHMARK_TIME_BEGIN
-/*    DEBUG_SC_EVENT("Simulator time stamp at end " << sc_time_String_Get(sc_time_stamp()));
-    DEBUG_SC_EVENT("Simulator begin " << sc_time_String_Get(SC_t));
-    DEBUG_SC_EVENT("Simulator length " << sc_time_String_Get(SC_x));
-    std::cerr  << "  -Elapsed for running the simulator: " << x.count()/1000/1000. << " msec CLOCK time" << "//" << sc_time_String_Get(SC_s, SC_TIME_UNIT_DEFAULT) << " " << SC_TIME_UNIT[SC_TIME_UNIT_DEFAULT] << " SIMULATED time" << endl;
- */   if (MoreEvents)
-        DEBUG_SC_PRINT(" Running continued in period [" << sc_time_String_Get(SC_t) << "," << sc_time_String_Get(SC_x) << "]");
-#endif
-
-    return MoreEvents;  // If we shall come back again
+    BENCHMARK_TIME_END(&t,&x,&s);
+    SC_BENCHMARK_TIME_END(&SC_t,&SC_x,&SC_s);
+    return mMoreEvents;  // If we shall come back again
 }
+//^^    DEBUG_SC_PRINT(" Running continued in period [" << sc_time_String_Get(SC_OldT) << "," << sc_time_String_Get(SC_s) << "]");
 
 // Register only already created modules !!!
 void scGenComp_Simulator::RegisterPU(scGenComp_PU_Bio* Module)
 {
     scGenComp_PU_Abstract* Valid = dynamic_cast<scGenComp_PU_Abstract*>(Module);
-    if(!Valid) return;    // Attempting to register something wrong
+    assert(Valid);    // Attempting to register something wrong
     mWatchedPUs.push_back(Module);
-    DEBUG_SC_PRINT("Module " << Module->name() << " registered");
+    Module->RegisterSimulator(this);
+            DEBUG_SC_PRINT("Module " << Module->name() << " registered");
 }
 
 void  scGenComp_Simulator::Update(void)
 {
-    int32_t AA = 0;
-    mWatchedPUs[0]->GetData(AA);
+    if(!mUpdateUnit) return;    // The event is not observed
+    switch(mUpdateObservingBit)
+        {
+        default: assert(0); break;
+        case gcob_ObserveProcessingBegin: //Watch 'Begin Computing'
+            UpdateProcessingBegin(mUpdateUnit);
+            break;
+        case gcob_ObserveProcessingEnd:  // Watch 'End Computing'
+            UpdateProcessingEnd(mUpdateUnit);
+            break;
+        case gcob_ObserveHeartbeat:     // Observe 'Heartbeat's of the PU
+            UpdateHeartbeat(mUpdateUnit);
+            break;
+        case gcob_ObserveInput:         // Observe 'Input's of the PU
+            UpdateInput(mUpdateUnit);
+            break;
+        case gcob_ObserveInitialize:         // Observe 'Initialize's of the PU
+            UpdateInitialize(mUpdateUnit);
+            break;
+            // just maintains the number of bits used
+        }
+        mUpdateUnit = (scGenComp_PU_Abstract*)NULL;
+ }
+
+// Update input information in simulator
+void  scGenComp_Simulator::UpdateInput(scGenComp_PU_Abstract* PU)
+{
+    DEBUG_SC_EVENT(PU->name()<< " #inputs " << PU->NoOfInputsReceived_Get());
+}
+
+// Update initialize information in simulator
+void  scGenComp_Simulator::UpdateInitialize(scGenComp_PU_Abstract* PU)
+{
+    DEBUG_SC_EVENT(PU->name() << " Inited");
+}
+// Update ''ProcessingBegin' information in simulator
+void  scGenComp_Simulator::UpdateProcessingBegin(scGenComp_PU_Abstract* PU)
+{
+    DEBUG_SC_EVENT(PU->name()<< " ProcessingBegin");
+}
+// Update ''ProcessingEnd' information in simulator
+void  scGenComp_Simulator::UpdateProcessingEnd(scGenComp_PU_Abstract* PU)
+{
+    DEBUG_SC_EVENT(PU->name() << " ProcessingEnd");
+}
+// Update 'Heartbeat' information in simulator
+void  scGenComp_Simulator::UpdateHeartbeat(scGenComp_PU_Abstract* PU)
+{
+    DEBUG_SC_EVENT(PU->name() << " Heartbeat");
+}
+void scGenComp_Simulator::
+    Observe(scGenComp_PU_Abstract* PU, GenCompPUObservingBits_t B)
+{
+    assert(PU);     // not installed
+    assert(B<gcob_Max); // wrong bit
+    if(PU->ObservingBit_Get(gcob_ObserveModule))
+    {   // Everything OK, the module observed
+        mUpdateUnit = PU; mUpdateObservingBit = B;
+    }
+    else
+    {   //No, the in not observed
+        mUpdateUnit = (scGenComp_PU_Abstract*) NULL;
+    }
 }
