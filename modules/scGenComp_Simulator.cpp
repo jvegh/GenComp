0d7e15301699bd8102d3449fc8a67bd7004da0c5
jvegh <Vegh.Janos@gmail.com>
2024-01-19 12:24:51 +0100

    Update moved to the caller Addad more observe conditions

--- modules/scGenComp_Simulator.cpp
+++ modules/scGenComp_Simulator.cpp
@@ -55,7 +55,6 @@ bool scGenComp_Simulator::Run(GenCompSimulatorModes_t SimulatorMode, int32_t Sim
     {   // We are in the process of simulating
         sc_start( sc_time_to_pending_activity());   // Run up to the next activity
         mMoreEvents = sc_pending_activity();    // Check if we have anything to do
-        Update();   // Do updating if anything is to be updated
         if((MoreCycles))
         {   // We have still more events, see which mode requested
             switch(SimulatorMode)
@@ -81,7 +80,7 @@ bool scGenComp_Simulator::Run(GenCompSimulatorModes_t SimulatorMode, int32_t Sim
 //^^    DEBUG_SC_PRINT(" Running continued in period [" << sc_time_String_Get(SC_OldT) << "," << sc_time_String_Get(SC_s) << "]");
 
 // Register only already created modules !!!
-void scGenComp_Simulator::RegisterPU(scGenComp_PU_Bio* Module)
+void scGenComp_Simulator::RegisterPU(scGenComp_PU_Abstract* Module)
 {
     scGenComp_PU_Abstract* Valid = dynamic_cast<scGenComp_PU_Abstract*>(Module);
     assert(Valid);    // Attempting to register something wrong
@@ -93,7 +92,7 @@ void scGenComp_Simulator::RegisterPU(scGenComp_PU_Bio* Module)
 void  scGenComp_Simulator::Update(void)
 {
     if(!mUpdateUnit)
-    {   DEBUG_SC_EVENT("Nothing to update");
+    {   //DEBUG_SC_EVENT("Nothing to update");
         return;    // We  have no new update request
     }
     switch(mUpdateObservingBit) // Seem if the activity itself is not observed
@@ -101,6 +100,12 @@ void  scGenComp_Simulator::Update(void)
         default: assert(0); break;
         case gcob_ObserveProcessingBegin: //Watch 'Begin Computing'
             UpdateProcessingBegin(mUpdateUnit);
+            break;
+        case gcob_ObserveDeliveringBegin: //Watch 'Begin Delivering'
+            UpdateDeliveringBegin(mUpdateUnit);
+            break;
+        case gcob_ObserveDeliveringEnd: //Watch 'End Delivering'
+            UpdateDeliveringEnd(mUpdateUnit);
             break;
         case gcob_ObserveProcessingEnd:  // Watch 'End Computing'
             UpdateProcessingEnd(mUpdateUnit);
@@ -114,7 +119,12 @@ void  scGenComp_Simulator::Update(void)
         case gcob_ObserveInitialize:         // Observe 'Initialize's of the PU
             UpdateInitialize(mUpdateUnit);
             break;
-            // just maintains the number of bits used
+        case gcob_ObserveRelaxingBegin: //Watch 'Begin Relaxing'
+            UpdateRelaxingBegin(mUpdateUnit);
+            break;
+        case gcob_ObserveRelaxingEnd: //Watch 'End Relaxing'
+            UpdateRelaxingEnd(mUpdateUnit);
+            break;
         }
         mUpdateUnit = (scGenComp_PU_Abstract*)NULL;
  }
@@ -135,8 +145,31 @@ void  scGenComp_Simulator::UpdateProcessingBegin(scGenComp_PU_Abstract* PU)
 {
     DEBUG_SC_PRINT(PU->name()<< " ProcessingBegin");
 }
+// Update ''RelaxingBegin' information in simulator
+void  scGenComp_Simulator::UpdateRelaxingBegin(scGenComp_PU_Abstract* PU)
+{
+    DEBUG_SC_PRINT(PU->name()<< " RelaxingBegin");
+}
+
+// Update 'DeliveringBegin' information in simulator
+void  scGenComp_Simulator::UpdateDeliveringBegin(scGenComp_PU_Abstract* PU)
+{
+    DEBUG_SC_PRINT(PU->name()<< " DeliveringBegin");
+}
+
+// Update 'DeliveringEnd' information in simulator
+void  scGenComp_Simulator::UpdateDeliveringEnd(scGenComp_PU_Abstract* PU)
+{
+    DEBUG_SC_PRINT(PU->name()<< " DeliveringEnd");
+}
+
 // Update ''ProcessingEnd' information in simulator
 void  scGenComp_Simulator::UpdateProcessingEnd(scGenComp_PU_Abstract* PU)
+{
+    DEBUG_SC_PRINT(PU->name() << " ProcessingEnd");
+}
+// Update ''RelaxingEnd' information in simulator
+void  scGenComp_Simulator::UpdateRelaxingEnd(scGenComp_PU_Abstract* PU)
 {
     DEBUG_SC_PRINT(PU->name() << " ProcessingEnd");
 }
