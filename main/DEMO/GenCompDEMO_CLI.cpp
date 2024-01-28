ad73e5037c8bdcfdb2047a99d8b2e54b40ec8c6d
jvegh <Vegh.Janos@gmail.com>
2024-01-28 18:44:22 +0100

    Time benchmarking reorganized

--- main/DEMO/GenCompDEMO_CLI.cpp
+++ main/DEMO/GenCompDEMO_CLI.cpp
@@ -36,6 +36,15 @@
 //??scClusterBusSimulator* TheSimulator;
 //??string ListOfIniFiles;
 
+/**
+ * The system maintains the following time values:
+ * SIMULATED time: the value between starting (or reseting) the simulation and now
+ * CLOCK time:
+ *  - SystemC processor time: the time used for driving the SystemC engine
+ *  - Display time: the non-SystemC related processor time, mainly GUI
+ *  - User time: time spent with waiting for the user
+ */
+
 #include "scGenComp_PU_AbstractDEMO.h"
 #include "scGenComp_PU_BioDEMO.h"
 #include "scGenComp_PU_Bio_IzhikevichDEMO.h"
@@ -50,6 +59,7 @@ scGenComp_PU_Bio_IzhikevichDEMO* MyBioIzhikevichDEMO;
 scGenComp_Simulator* MySimulator;
 
 // Prepare sxXXX modules and instantiate them
+// When using without scGenComp_Simulator, instantiate the objectyou want to use
 int32_t scPrepareGenCompObjects(int32_t ObjectSelector)
 {
     switch(ObjectSelector)
@@ -67,7 +77,6 @@ int32_t scPrepareGenCompObjects(int32_t ObjectSelector)
     return 0;
 }
 
-bool UNIT_TESTING = false; // Used internally for debugging
 //QTextEdit *Simulator_LogWindow = 0; // By default and for CLI, we have no QTextEdit
 
 // Using sc_main() is mandatory for using SystemC; equivalent with main()
@@ -78,25 +87,25 @@ int sc_main(int argc, char* argv[])
     // We rely on the default clearing of the values of time benchmarking
     chrono::steady_clock::time_point t;
     std::chrono::duration<int64_t, nano> x,s=(std::chrono::duration< int64_t, nano>)0;
+    chrono::steady_clock::time_point D_t;
+    std::chrono::duration<int64_t, nano> D_x,D_s=(std::chrono::duration< int64_t, nano>)0;
     sc_core::sc_time SC_t, SC_x, SC_s;
-    BENCHMARK_TIME_RESET(&t,&x,&s);
-    SC_BENCHMARK_TIME_RESET(&SC_t,&SC_x,&SC_s);
     sc_core::sc_report_handler::set_actions( "/IEEE_Std_1666/deprecated",
                                             sc_core::SC_DO_NOTHING );
     // About to start
     std::cerr  << "\n Demonstrates using GenComp with SystemC" << "\n>>> Entering " << PROJECT_NAME << "_DEMO/CLI V"
               << PROJECT_VERSION;
     if(UseSimulator)
-        std::cout << " with simulator";
-    std::cout << endl;
+        std::cerr << " with simulator";
+    std::cerr << endl;
 
-    // Do whatever setup you will need for your program here
+    BENCHMARK_TIME_RESET(&t,&x,&s);
+    SC_BENCHMARK_TIME_RESET(&SC_t,&SC_x,&SC_s);
+   // Do whatever setup you will need for your program here
     //
     // You may use your object without a frame (without using a simulator)
     // Just prepare your sc_modules in a wrapper; the functionality is defined in its Initialize method
      // ------Make anything before starting
-    BENCHMARK_TIME_BEGIN(&t,&x);
-    SC_BENCHMARK_TIME_BEGIN(&SC_t,&SC_x);
     if(!UseSimulator)
     {
         // !! all SC-related object and connections must be established before calling the simulator!!
@@ -118,10 +127,13 @@ int sc_main(int argc, char* argv[])
     }
     BENCHMARK_TIME_END(&t,&x,&s);
     SC_BENCHMARK_TIME_END(&SC_t,&SC_x,&SC_s);
+    // Save preparation time for summary
+    sc_core::sc_time scPrepare = SC_s;
+    std::chrono::duration<int64_t, nano> sPrepare = s;
     // ------!!No instantiation of objects from classes scXXX is allowed outside this section!!
     std::cerr  << "  -Elapsed for preparing simulation: " << x.count()/1000/1000. << " msec CLOCK time" << "//" << sc_time_String_Get(SC_s,SC_TIME_UNIT_DEFAULT) << " " << SC_TIME_UNIT[SC_TIME_UNIT_DEFAULT] << " SIMULATED time" << endl;
 
-    if(!returnValue)
+     if(!returnValue)
     {   // It was all right with the preparation, prepare simulation
         //  Set up here the simulator
         BENCHMARK_TIME_BEGIN(&t,&x);
@@ -129,34 +141,36 @@ int sc_main(int argc, char* argv[])
         if(!UseSimulator)
             sc_start(); // Will run until dies
         else
-        {   // This is the real simulation cycle: runs until end
+        {
+            MySimulator->Enable.notify(SC_ZERO_TIME);   // Everything prepared, enable simulator
+            // This is the real simulation cycle: runs until end
             // but allows updating any state of the simulated object
             while(MySimulator->Run(gcsm_Eventwise,1))
             {   // This portion is run repeatedly by the simulator
                 MySimulator->Update();   // Do updating if anything is to be updated
                 // Update display here
             }
-//            DEBUG_SC_LOG("Processing time was: " << sc_time_String_Get(sc_core::sc_timeStamp(),SC_TIME_UNIT_DEFAULT));
-
         };
         BENCHMARK_TIME_END(&t,&x,&s);
         SC_BENCHMARK_TIME_END(&SC_t,&SC_x,&SC_s);
-        std::cerr  << "  -Elapsed for running the simulation: " << MySimulator->TimeToProcess_Get().count()/1000/1000. << " msec CLOCK time" << "//" << sc_time_String_Get(SC_s, SC_TIME_UNIT_DEFAULT)
-                  << " " << SC_TIME_UNIT[SC_TIME_UNIT_DEFAULT] << " SIMULATED time" << endl;
     }
     else
         std::cerr  << "  -Error " << returnValue << " during preparing objects" << endl;
 
-     std::cerr  << "<<< Exiting " << PROJECT_NAME << "_DEMO/CLI V" << PROJECT_VERSION;
+    std::cerr  << "<<< Exiting " << PROJECT_NAME << "_DEMO/CLI V" << PROJECT_VERSION;
     if(returnValue)
         std::cerr << ", error code " << returnValue << endl;
     else
         std::cerr << " with no error"  << endl;
     if(UseSimulator)
-        std::cerr  << "GenComp/DEMO by scGenComp_Simulator " << MySimulator->TimeToProcess_Get().count()/1000/1000. << " msec CLOCK time" << "//"
-                  <<  sc_time_String_Get(MySimulator->scTime_Get(), SC_TIME_UNIT_DEFAULT) << " " << SC_TIME_UNIT[SC_TIME_UNIT_DEFAULT] << " SIMULATED time" << endl;
-    else
-        std::cerr  << "GenComp/DEMO simulation " << s.count()/1000/1000. << " msec CLOCK time" << "//" << sc_time_String_Get(SC_s, SC_TIME_UNIT_DEFAULT)
+        std::cerr  << "  -Elapsed for running the simulation:  CLOCK/SIMULATED, in " << SC_TIME_UNIT[SC_TIME_UNIT_DEFAULT] << endl
+                  << "TOTAL:  " << s.count()/1000/1000. << "/" << sc_time_String_Get(SC_s, SC_TIME_UNIT_DEFAULT) << endl
+                  << "PREPARE:" << sPrepare.count()/1000/1000. << "/" << sc_time_String_Get(scPrepare, SC_TIME_UNIT_DEFAULT) << endl
+                  << "PROCESS:" << MySimulator->TimeToProcess_Get().count()/1000/1000. << "/"  << sc_time_String_Get(SC_s, SC_TIME_UNIT_DEFAULT) << endl
+                  << "DISPLAY:" <<MySimulator->TimeToDisplay_Get().count()/1000/1000. << "/" << endl
+                  << "USER:   " <<(s-MySimulator->TimeToDisplay_Get()-MySimulator->TimeToProcess_Get()).count()/1000/1000. << "/" << endl;
+     else
+        std::cerr  << "GenComp/DEMO " << s.count()/1000/1000. << " msec CLOCK time" << "//" << sc_time_String_Get(SC_s, SC_TIME_UNIT_DEFAULT)
                    << " " << SC_TIME_UNIT[SC_TIME_UNIT_DEFAULT] << " SIMULATED time" << endl;
     return(returnValue);
 }
