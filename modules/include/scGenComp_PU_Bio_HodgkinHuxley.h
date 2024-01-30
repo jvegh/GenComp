be809fd26581a208ebba81465e307ff703327d09
jvegh <Vegh.Janos@gmail.com>
2024-01-30 11:11:29 +0100

    Started to implement Hodgkin-Huxley

---
+++ modules/include/scGenComp_PU_Bio_HodgkinHuxley.h
@@ -0,0 +1,256 @@
+/** @file scGenComp_PU_Bio_HodgkinHuxley.h
+ *  @ingroup GENCOMP_MODULE_BIOLOGY
+
+ *  @brief Implements Hodgkin-Huxley model of neuronal operation
+ *  @todo Implement the correct DEs
+ */
+/*
+ *  @author János Végh (jvegh)
+ *  @bug No known bugs.
+*/
+
+#ifndef SCBIOGENCOMPHODGKINHUXLEY_H
+#define SCBIOGENCOMPIZHIKEVICH_H
+/** @addtogroup GENCOMP_MODULE_PROCESS
+ *  @{
+ */
+
+#include "scGenComp_PU_Bio.h"
+
+/*
+ * \class scGenComp_PU_Bio_HodgkinHuxley
+ * \brief  A simple abstract class to implement the operation of a general bio computing unit
+ *
+ * Operating principle @see scGenComp_PU_Bio
+ *
+ * Some virtual methods overwrite the general "Bio-Computing" behavior, according to HodgkinHuxley's model
+ *
+ * The rest of methods are of technical nature.
+ * - HeartBeat: integrate HodgkinHuxley's PDE
+  */
+
+/*!
+ * \class scGenComp_PU_Bio_HodgkinHuxley
+ * \brief  Implements the HodgkinHuxley-type biological computing PU
+     * @param[in] nm the SystemC name of the module
+     * @param[in] Heartbeat the state refresh time
+     *
+     * Creates  an HodgkinHuxley-type biological computing unit:
+     *
+     * [1] A. L. Hodgkin and A. F. Huxley, A quantitative description of membrane current
+     * and its application to conduction and excitation in nerve
+     * J. Physiol. (1952) 117:500-544
+
+     *
+     *  Hodgkin-Huxley model:
+     *  \f[ \frac{dv}{dt} = 0.04*v^2 + 5*v + 140 - u + I \f]
+     *  \f[ \frac{du}{dt} = a( b*v - u) \f]
+     *   - \f$ v \f$ the membrane potential (in mV)
+     *   - \f$ u \f$ the membrane recovery potential (in mV)
+     *   - \f$ t \f$ time (in msec, the local time)
+     *   - \f$ a \f$ the time scale of the recovery variable \f$ u \f$. (0.02) Smaller values result in slower recovery.
+     *   - \f$ b \f$ the sensitivity of the recovery variable \f$ u \f$ to the subthreshold fluctuations of the membrane potential \f$ v \f$
+     *   - \f$ c \f$ after-spike reset value of the membrane potential v. (c = 065 mV)
+     *   - \f$ d \f$ after-spike reset of the recovery variable of\f$ u \f$ (d=2)
+     *
+     *   Parameters:
+     *  - V_m        double - Membrane potential in mV
+     *  - U_m        double - Membrane potential recovery variable
+     *  - V_th       double - Spike threshold in mV.
+     *  - I_In        double - Constant input current in pA. (R=1)
+     *  - V_min      double - Absolute lower value for the membrane potential.
+     *  - a          double - describes time scale of recovery variable
+     *  - b          double - sensitivity of recovery variable
+     *  - c          double - after-spike reset value of V_m
+     *  - d          double - after-spike reset value of U_m
+     *  - AsPublished  bool - use as published, with omitting statios 'Delivering' and 'Relaxing'
+     *   References:
+     *
+     *   Statios: In different stages the parameters I and V_th are interpreted differently.
+     *  - In 'Processing' statio, I is the sum of the synaptic input current plus the introduced direct current:
+     *   the PU counts, and sums up the charge received in these two ways, until its threshold voltage V_th reached.
+     *   Then the PU passes to 'Delivering' statio: the result is ready, we must deliver it to the hillock.
+     *   The ion channels get closed. no more synaptic ionic input and the threshold is about the minimum+25 mV
+     *  - In 'Delivering' mode, the I input current is what Na+ and K+ ions from the extracellular space contribute and is much higher than
+     *   in 'processing' mode; however no synaptic and direct input. The state is terminated when potential +130 mV reached.
+     *   At that point, the spike starts and relaxing begins; passes to 'Relaxing' statio
+     *  - In 'Relaxing' statio, some negative ionic current from the extracellular space is present and the
+     *   membrane potential decreases below the resting potential. Now the neuron passes to statio 'Ready'.
+     *  - in 'Ready' statio, the the ioninc channels are open, but the voltage decays towards the resting potential.
+     *   Any input current takes the system to 'Processing, see above.
+     *
+     *   in the original model, statios 'Processing' and 'Relaxing' are contracted and described by the same function,
+     *   'Relaxing' statio is dropped (time skip) and replaced by a drascic and sudden change in v_M
+     *
+     *   The modified model separates 'Processing' and 'Delivery', introduces a 'Relaxing' statio that has no current input
+     *   and relaxes in the same way as the original in 'Ready' statio. The station passes to 'Ready' when the original
+     *   membrane cutback point reached.
+     *
+     * Code uses a Nest fragment
+     */
+
+class scGenComp_PU_Bio_HodgkinHuxley : public scGenComp_PU_Bio
+{
+  public:
+    scGenComp_PU_Bio_HodgkinHuxley(sc_core::sc_module_name nm   // Module name
+                   ,bool AsPublished // if with omitting statios 'delivering' and sRelaxing'
+                   ,sc_core::sc_time Heartbeat);  // Heartbeat time
+    virtual ~scGenComp_PU_Bio_HodgkinHuxley(void); // Must be overridden
+
+    /**
+     * @brief A new spike received; legal only in 'Ready' and 'Processing' states
+     *
+     * Called by scGenComp_PU_Abstract::InputReceived_method.
+     * A spike arrived, store spike parameters. Receving an input is a momentary action, just administer its processing.
+     * If it was the first spike, issue 'Begin_Computing'
+     *
+     * Reimplemented given that in biology the first input also starts processing
+     * Receiving input is recently just symbolic, no details elaborated
+     */
+    virtual void InputReceived_Do();
+
+    /* *
+     * @brief Called when the state 'Relaxing' begins
+     *
+      */
+//    virtual void RelaxingBegin_Do();
+
+    /**
+     * @brief Called when the state 'Relaxing' ends
+     *
+     * The unit passes to phase 'Ready'
+     */
+    virtual void RelaxingEnd_Do(){}
+
+    /**
+     * @brief Heartbeat_method
+     *
+     * A periodic  signal as a timebase for solving differential equations.
+     *
+     * - The unit receives a signal EVENT_GenComp.HeartBeat and calculates the membran potential.
+     *
+     *   Most of the job is done in methods Heartbeat_Processing_Do() (This was elaborated first),
+     * Heartbeat_Delivering_Do() and  Heartbeat_Relaxing_Do() and Heartbeat_Ready_Do().
+     * These Heartbeat_XXX_Do() methods work on the same principle, only their function and terminating condition are different
+     * (furthermore, the station to which they pass). They issue the corresponding event to pas to the next statio.
+
+     * - In 'Processing' mode, re-calculates membrane's charge-up potential
+     * - In 'Ready' mode, re-calculates membrane's decay potential
+     * - In 'Delivering' mode, re-calculates membrane's decay potential
+     */
+
+
+  protected:
+     /*
+     * Handle heartbeats in 'Ready' mode
+     */
+    /**
+     * @brief Handle heartbeats in 'Processing' mode
+     *
+     * -  Re-issues EVENT_GenComp.Heartbeat until membrane potential reaches its threshold value
+     * -  Starts to receive heartbeats after EVENT_GenComp.ProcessingBegin arrives
+     * -  Stops generating heartbeats and issues EVENT_GenComp.ProcessingEnd
+     * -  Issues EVENT_GenComp.Fail whey the membrane potential decaya near to its threshold
+     */
+    virtual void Heartbeat_Processing_Do();
+
+    /**
+     * @brief Handle heartbeats in 'Delivering' mode
+     *
+     * -  Re-issues EVENT_GenComp.Heartbeat until membrane potential at hillock reaches its peak value
+     * -  Starts to receive heartbeats after EVENT_GenComp.DeliveringBegin arrives
+     * -  Stops generating heartbeats and issues EVENT_GenComp.DeliveringEnd
+     */
+    void Heartbeat_Delivering_Do();
+
+    /**
+     * @brief Handle heartbeats in 'Ready' mode
+     *
+     * -  Re-issues EVENT_GenComp.Heartbeat until membrane potential reaches its recovery value
+     * -  Starts to receive heartbeats after EVENT_GenComp.ReadyBegin arrives
+     * -  Stops generating heartbeats and issues EVENT_GenComp.ReadyEnd
+     */
+    void Heartbeat_Ready_Do();
+
+    /**
+     * @brief Handle heartbeats in 'Relaxing' mode
+     *
+     * -  Re-issues EVENT_GenComp.Heartbeat until membrane potential starts to increase
+     * -  Starts to receive heartbeats after EVENT_GenComp.RelaxingBegin arrives
+     * -  Stops generating heartbeats after EVENT_GenComp.DeliveringEnd  arrives
+     *     then issues EVENT_GenComp.Initializing
+     */
+     void Heartbeat_Relaxing_Do();
+
+    /**
+     * @brief SolvePDE : solve the differential equations for the actual time increment
+     *
+     *  HodgkinHuxley model:
+     *  \f[ \frac{dv}{dt} = 0.04*v^2 + 5*v + 140 - u + I \f]
+     *  \f[ \frac{du}{dt} = a( b*v - u) \f]
+     *   - \f$ v \f$ the membrane potential (in mV)
+     *   - \f$ u \f$ the membrane recovery potential (in mV)
+     *   - \f$ t \f$ time (in msec, the local time)
+     *   - \f$ a \f$ the time scale of the recovery variable \f$ u \f$. (0.02) Smaller values result in slower recovery.
+     *   - \f$ b \f$ the sensitivity of the recovery variable \f$ u \f$ to the subthreshold fluctuations of the membrane potential \f$ v \f$
+     *   - \f$ c \f$ after-spike reset value of the membrane potential v. (c = 065 mV)
+     *   - \f$ d \f$ after-spike reset of the recovery variable of\f$ u \f$ (d=2)
+     *
+     *  The state of the biological computing is re-calculated (as the simulation time passes)
+     *  (solve the differential equation at this time; reset takes place in RelaxingBegin_Do )
+     *  @see scGenComp_PU_Bio#RelaxingBegin_Do
+     */
+     /**
+     * @brief Threshold_Exceeded
+     * @return true if spike generation threshold exceeded and 'Delivering'  follows
+     */
+    bool ThresholdExceeded(void);
+
+    /**
+     * @brief Peak_Exceeded
+     * @return true if spike generation threshold exceeded and 'Relaxing' follows
+     */
+    bool PeakExceeded(void);
+
+    /**
+     * @brief Resting potential approached
+     * @return true if potential approached mV_M to < m_C + m_D
+     */
+    bool RestingApproached(void);
+
+    /**
+     * @brief Resting potential reached
+     */
+    bool RestingReached(void);
+
+    /**
+     *
+     * @brief InputCurrent_Set
+     * @param[in] I The current, in pA
+     */
+    void InputCurrent_Set(int I) {mI_In = I;}
+    int InputCurrent_Get(void) {return mI_In;}
+    /**
+     * @brief UseAsPublished_Set
+     * @param[in] B if to use the method as published
+     *
+     * As published, the method uses no delivery and refractory periods
+     */
+    void UseAsPublished_Set(bool B) { mAsPublished = B;}
+    bool UseAsPublished_Get(void){return mAsPublished;}
+protected:
+    double mTimeStep    //As documented in Nest
+        ,mV_M    // V_m        double - Membrane potential in mV
+        ,mU_R    // U_m        double - Membrane potential recovery variable (0.) mV
+        ,mV_Th   // V_th       double - Spike threshold in mV (30.) mV
+        ,mI_In   // I_e        double - Constant input current in pA. (R=1)(0.)
+        ,mV_Min  // V_min      double - Absolute lower value for the membrane potential (-75) mV
+        ,m_A     // a          double - describes time scale of recovery variable (0.02)
+        ,m_B     // b          double - sensitivity of recovery variable(0.2)
+        ,m_C     // c          double - after-spike reset value of V_m (-65.)
+        ,m_D;    // d          double - after-spike reset value of U_m(8.)
+        bool mAsPublished;
+  };// of class scGenComp_PU_Bio_HodgkinHuxley
+/** @}*/
+
+#endif // SCBIOGENCOMPIZHIKEVICH_H
---
+++ modules/scGenComp_PU_Bio_HodgkinHuxley.cpp
@@ -0,0 +1,302 @@
+/** @file scGenComp_PU_Bio_HodgkinHuxley.cpp
+ *  @ingroup GENCOMP_MODULE_BIOLOGY
+ *  @brief   Implements Hodgkin-Huxley's model
+ */
+/*
+ *  @author János Végh (jvegh)
+ *  @bug No known bugs.
+ */
+
+#include "scGenComp_PU_Bio_HodgkinHuxley.h"
+// This section configures debug and log printing; must be located AFTER the other includes
+//#define SUPPRESS_LOGGING // Suppress all log messages
+#define DEBUG_EVENTS    ///< Print event debug messages  for this module
+#define DEBUG_PRINTS    ///< Print general debug messages for this module
+// Those defines must be located before 'DebugMacros.h", and are undefined in that file
+#define DEBUG_DISABLED
+#include "DebugMacros.h"
+
+extern bool UNIT_TESTING;	// Whether in course of unit testing
+
+// Implement Hodgkin-Huxley model
+    scGenComp_PU_Bio_HodgkinHuxley::
+scGenComp_PU_Bio_HodgkinHuxley(sc_core::sc_module_name nm
+                                ,bool AsPublished
+                ,sc_core::sc_time Heartbeat ):  // Heartbeat time
+    scGenComp_PU_Bio(nm, Heartbeat)
+    ,mV_M (-65)  //mV, membrane potential -65*random[0,1]
+    ,mU_R (0.)   //mV membrane recovery variable, m_B * mV_M
+    ,mV_Th (30.) //mV; fixed 30
+    ,mI_In(40.) //mV*/R [5*random[0,1]-2*random[0,1]
+    ,mV_Min (-70.)  // Minimum membrane voltage
+    ,m_A(0.02)      // [0.02,0.1] * random[0,1]
+    ,m_B(0.2)       // [0.2,0.27] * random[0,1]
+    ,m_C(-65.)      // [-65,-50] + 15*random[0,1]^2
+    ,m_D(8.)        // [0.05,8] - 6*random[0,1]^2
+    ,mAsPublished(AsPublished)    // If to use without 'Relaxing' and 'Delvering'
+{
+    typedef scGenComp_PU_Bio_HodgkinHuxley SC_CURRENT_USER_MODULE;
+    double RefinedTime = HeartbeatTime_Get().to_seconds()*1000; // Have the time step refinement in ms
+    mTimeStep = RefinedTime/HeartbeatDivisions_Get();
+    // *** Do not reimplement any of the xxx_method functions
+    // *** until you know what you are doing. Do what you want in methods xxx_Do
+}
+    scGenComp_PU_Bio_HodgkinHuxley::
+~scGenComp_PU_Bio_HodgkinHuxley(void)
+{
+}
+
+
+/*
+     * Handle heartbeats in 'Delivering' mode
+     */
+void scGenComp_PU_Bio_Izhikevich::
+    Heartbeat_Delivering_Do()
+{
+    double LocalTime = scLocalTime_Get().to_seconds()*1000; // Have the time in ms
+    double MyCurrent;
+    if(mAsPublished)
+    {   // Use with the input current
+        MyCurrent = mI_In;
+    }
+    else
+    {   // Use charge from the extracellular space. temporary
+        MyCurrent = 5*mI_In;
+    }
+    for(int32_t i = 0; (i < mHeartbeatDivisions) and !PeakExceeded(); i++)
+    {
+        // ** This part solves the PDE
+        double OldV = mV_M;
+        double OldU = mU_R;
+
+        mV_M += mTimeStep * ( 0.04 * OldV * OldV + 5.0 *OldV + 140.0 - OldU + MyCurrent ); //??+ spike contrib
+        mU_R += mTimeStep * m_A * (m_B * OldV - OldU);
+        // check lower bound of membrane potential
+        if(mV_M<mV_Min)
+            mV_M = mV_Min; // Restore membrane potential
+        // ** end of pDE solving
+        DEBUG_SC_EVENT_LOCAL("@" << LocalTime+ i*mTimeStep <<  "; V(memb)=" << mV_M << ", U(rec)=" << mU_R << ", N=" << i);
+    }
+    if (PeakExceeded())
+    {   // We are about finishing processing
+        EVENT_GenComp.DeliveringEnd.notify(SC_ZERO_TIME);
+        DEBUG_SC_EVENT_LOCAL("SENT    EVENT_GenComp.DeliveringEnd");
+    }
+    else
+    {   // We are still processing; re-issue the heartbeat
+        // if the limit is not yet reached
+        EVENT_GenComp.Heartbeat.notify(mHeartbeat);
+        DEBUG_SC_EVENT_LOCAL("SENT    EVENT_GenComp.HeartBeat with " << sc_time_String_Get(mHeartbeat, SC_TIME_UNIT_DEFAULT)<< " " << SC_TIME_UNIT[SC_TIME_UNIT_DEFAULT]);
+    }
+}
+
+
+
+    /*
+     * Handle heartbeats in 'Processing' mode
+     */
+void scGenComp_PU_Bio_Izhikevich::
+    Heartbeat_Processing_Do()
+{
+    double LocalTime = scLocalTime_Get().to_seconds()*1000; // Have the time in ms
+    for(int32_t i = 0; (i < mHeartbeatDivisions) and !ThresholdExceeded(); i++)
+    {
+        // ** This part solves the PDE
+            double OldV = mV_M;
+            double OldU = mU_R;
+
+            mV_M += mTimeStep * ( 0.04 * OldV * OldV + 5.0 *OldV + 140.0 - OldU + mI_In ); //??+ spike contrib
+            mU_R += mTimeStep * m_A * (m_B * OldV - OldU);
+            // check lower bound of membrane potential
+            if(mV_M<mV_Min)
+                mV_M = mV_Min; // Restore membrane potential
+        // ** end of pDE solving
+        DEBUG_SC_EVENT_LOCAL("@" << LocalTime+ i*mTimeStep <<  "; V(memb)=" << mV_M << ", U(rec)=" << mU_R << ", N=" << i);
+    }
+    if (ThresholdExceeded())
+    {   // We are about finishing processing
+        EVENT_GenComp.ProcessingEnd.notify(SC_ZERO_TIME);
+                    DEBUG_SC_EVENT_LOCAL("SENT    EVENT_GenComp.ProcessingEnd");
+    }
+    else
+    {   // We are still processing; re-issue the heartbeat
+           // if the limit is not yet reached
+        EVENT_GenComp.Heartbeat.notify(mHeartbeat);
+        DEBUG_SC_EVENT_LOCAL("SENT    EVENT_GenComp.HeartBeat with +" << sc_time_String_Get(mHeartbeat, SC_TIME_UNIT_DEFAULT) << " " << SC_TIME_UNIT[SC_TIME_UNIT_DEFAULT]
+                             << ", in " << HeartbeatDivisions_Get() << " divisions");
+    }
+}
+
+
+    /**
+     * Handle heartbeats in 'Relaxing' mode
+     */
+
+void scGenComp_PU_Bio_Izhikevich::
+    Heartbeat_Relaxing_Do()
+{
+    if(mAsPublished)
+    {   // Just restore potentials
+        mV_M = m_C; // Restore membrane potential
+        mU_R += m_D; // Restore recovery potential
+           // We are about finishing processing
+            EVENT_GenComp.RelaxingEnd.notify(SC_ZERO_TIME);
+            DEBUG_SC_EVENT_LOCAL("SENT    EVENT_GenComp.RelaxingEnd");
+            return;
+    }
+
+    double  MyCurrent = 10*mI_In;
+    // This is the updated processing method
+    double LocalTime = scLocalTime_Get().to_seconds()*1000; // Have the time in ms
+    for(int32_t i = 0; (i < mHeartbeatDivisions) and !RestingApproached(); i++)
+    {
+         // ** This part solves the PDE
+         double OldV = mV_M;
+         double OldU = mU_R;
+
+         mV_M += mTimeStep * ( //0.04 * OldV * OldV + 5.0 *OldV
+                     130
+                              + 140.0 - OldU - MyCurrent); // No input current
+         mU_R += mTimeStep * m_A * (m_B * OldV - OldU);
+         // check lower bound of membrane potential
+         if(mV_M<mV_Min)
+             mV_M = mV_Min; // Restore membrane potential
+         // ** end of pDE solving
+         DEBUG_SC_EVENT_LOCAL("@" << LocalTime+ i*mTimeStep <<  "; V(memb)=" << mV_M << ", U(rec)=" << mU_R << ", N=" << i);
+    }
+     if (RestingApproached())
+     {   // We are about finishing processing
+         EVENT_GenComp.RelaxingEnd.notify(SC_ZERO_TIME);
+         DEBUG_SC_EVENT_LOCAL("SENT    EVENT_GenComp.RelaxingEnd");
+     }
+     else
+     {   // We are still processing; re-issue the heartbeat
+         // if the limit is not yet reached
+         EVENT_GenComp.Heartbeat.notify(mHeartbeat);
+         DEBUG_SC_EVENT_LOCAL("SENT    EVENT_GenComp.HeartBeat with  " << sc_time_String_Get(mHeartbeat, SC_TIME_UNIT_DEFAULT)<< " " << SC_TIME_UNIT[SC_TIME_UNIT_DEFAULT]
+                                                                                                                                    << ", in " << HeartbeatDivisions_Get() << " divisions");
+     }
+}
+
+/**
+     * Handle heartbeats in 'Ready' mode
+     */
+
+void scGenComp_PU_Bio_Izhikevich::
+    Heartbeat_Ready_Do()
+{
+    if(mAsPublished)
+    {   // Nothing to do
+        mV_M = m_C; // Restore membrane potential
+        mU_R += m_D; // Restore recovery potential
+        {   // We are about finishing processing
+            EVENT_GenComp.RelaxingEnd.notify(SC_ZERO_TIME);
+            DEBUG_SC_EVENT_LOCAL("SENT    EVENT_GenComp.RelaxingEnd");
+        }
+    }
+    // This is the updated processing method
+    double LocalTime = scLocalTime_Get().to_seconds()*1000; // Have the time in ms
+    for(int32_t i = 0; (i < mHeartbeatDivisions) and !RestingApproached(); i++)
+    {
+        // ** This part solves the PDE
+        double OldV = mV_M;
+        double OldU = mU_R;
+
+        mV_M += mTimeStep * ( 0.04 * OldV * OldV + 5.0 *OldV + 140.0 - OldU - mI_In ); // No input current
+        mU_R += mTimeStep * m_A * (m_B * OldV - OldU);
+        // check lower bound of membrane potential
+        if(mV_M<mV_Min)
+            mV_M = mV_Min; // Restore membrane potential
+        // ** end of pDE solving
+        DEBUG_SC_EVENT_LOCAL("@" << LocalTime+ i*mTimeStep <<  "; V(memb)=" << mV_M << ", U(rec)=" << mU_R << ", N=" << i);
+    }
+    if (RestingApproached())
+    {   // We are about finishing processing
+        mV_M = m_C; // Restore membrane potential, as published
+        mU_R += m_D; // Restore recovery potential
+        DEBUG_SC_EVENT_LOCAL("End of processing, in ready again");
+    }
+    else
+    {   // We are still processing; re-issue the heartbeat
+        // if the limit is not yet reached
+        EVENT_GenComp.Heartbeat.notify(mHeartbeat);
+        DEBUG_SC_EVENT_LOCAL("SENT    EVENT_GenComp.HeartBeat with  " << sc_time_String_Get(mHeartbeat, SC_TIME_UNIT_DEFAULT)<< " " << SC_TIME_UNIT[SC_TIME_UNIT_DEFAULT]
+                                                                                                                                   << ", in " << HeartbeatDivisions_Get() << " divisions");
+    }
+}
+
+
+/*
+ * This routine makes actual input processing, although most of the job is done in Process() and Heartbeat()
+ * It can be called in state 'Processing' (if not first input)
+ * or in state 'Ready' if first input
+ *
+ * If it was the first spike, issue 'ComputingBegin' and re-issue
+ */
+void scGenComp_PU_Bio_Izhikevich::
+   InputReceived_Do(void)
+{
+    if(!((gcsm_Ready == mStateFlag) || (gcsm_Processing == mStateFlag))) return;
+    // inputs are processed only in 'Ready' and 'Processing' states
+    if(gcsm_Ready == mStateFlag)
+    {   // we are still in 'Ready' state
+        mStateFlag = gcsm_Processing;   // Be sure we do not repeat
+        EVENT_GenComp.ProcessingBegin.notify(SC_ZERO_TIME); // Put events in order
+                DEBUG_SC_EVENT_LOCAL("SEND EVENT_GenComp.ProcessingBegin");
+        EVENT_GenComp.InputReceived.notify(1,SC_PS); // Re-issue InputReceived
+                DEBUG_SC_EVENT_LOCAL("re-SEND EVENT_GenComp.InputReceived with 1 ps delay");
+    }
+    else
+    {
+//        DEBUG_SC_EVENT_LOCAL("RCVD EVENT_GenComp.InputReceived in mode '" << GenCompStatesString[mStateFlag] << "'");
+        scGenComp_PU_Bio::InputReceived_Do();
+    }
+ }
+
+
+
+bool scGenComp_PU_Bio_Izhikevich::
+    ThresholdExceeded(void)
+{
+    return mV_M>=mV_Min+25;   // threshold crossing
+}
+
+bool scGenComp_PU_Bio_Izhikevich::
+    PeakExceeded(void)
+{
+    return mV_M>=130;   // Peak voltage crossing
+}
+
+bool scGenComp_PU_Bio_Izhikevich::
+    RestingApproached(void)
+{
+      return mV_M< m_C+m_D;   // Restoration potential approached
+}
+bool scGenComp_PU_Bio_Izhikevich::
+    RestingReached(void)
+{
+    return mV_M< m_C+m_D/4;   // Restoration potential approached
+}
+
+/*
+ *     // threshold crossing
+    if ( S_.v_ >= P_.V_th_ )
+    {
+
+      // compute spike time
+      set_spiketime( Time::step( origin.get_steps() + lag + 1 ) );
+
+      SpikeEvent se;
+      kernel().event_delivery_manager.send( *this, se, lag );
+    }
+
+    // set new input current
+    S_.I_ = B_.currents_.get_value( lag );
+
+    // voltage logging
+    B_.logger_.record_data( origin.get_steps() + lag );
+  }
+*/
+
+
+
